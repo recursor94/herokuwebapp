6567bd64bcbfccf92df056cdd04c851c3cb661a0
Andrew Spano <andrew@localhost.localdomain>
2013-05-29 21:14:11 -0400

    fixed syntax error

--- src/shouter/models/shout.clj
+++ src/shouter/models/shout.clj
@@ -4,7 +4,7 @@
 
 (defn all []
   (sql/with-connection (System/getenv "DATABASE_URL")
-    (sql/with/query-results results)
+    (sql/with-query-results results)
     ["SELECT * FROM shouts ORDER BY ID DESC"]
     (into [] results)))
 
